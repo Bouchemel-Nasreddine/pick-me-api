8c62699974e93b0343c064d499c99cc146ba82df
Nasreddine Bouchemel <kn_bouchemel@esi.dz>
2022-07-31 19:53:25 +0100

    port prob2

--- index.js
+++ index.js
@@ -52,9 +52,9 @@ app.use((error, req, res, next) => {
 mongoose.connect(dbUrl).then(() => {
     console.log('Connected to MongoDB');
 }).then( () => {
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+    console.log('Server is running on port' + PORT);
 });
 } ).catch((err) => {
-    console.log(PORT);
+    console.log(err);
 });
